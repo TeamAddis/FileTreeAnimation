ca76ab4aee8bc4ff231f5007b8f7af6c01fac0bd
Andrew Addis <addisandrew1@gmail.com>
2013-04-26 01:01:17 -0400

    added git log script

---
+++ gitLogToXML.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+name="${PWD##*/}"
+
+revlist=$(git rev-list --reverse HEAD)
+(
+	echo '<?xml version="1.0"?>'
+	echo '<git name=$name>'
+	declare -i id=0
+	for rev in $revlist
+	do
+		sid=$id
+		q='"'
+		echo "<commit id=$q$sid$q>"
+
+		files=$(git log -1 --pretty="format:" --name-only $rev)
+		for file in $files
+		do
+			echo "	<file>$file</file>"
+		done
+
+		echo '</commit>'
+		id+=1
+	done
+
+	echo '</git>'
+) > data/commits.xml
+
